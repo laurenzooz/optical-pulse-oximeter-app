0fc7547bca870b30c2884b310c755efdce89fba3
laurenzooz <lauri.pahkala@hotmail.com>
2024-11-10 15:59:12 +0200

    remove debug logs

--- src/oximeterapp/lib/main.dart
+++ src/oximeterapp/lib/main.dart
@@ -5,44 +5,6 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:universal_ble/universal_ble.dart';
 
-class ResultWidget extends StatelessWidget {
-  final List<String> results;
-  final Function(int?) onClearTap;
-
-  const ResultWidget({
-    Key? key,
-    required this.results,
-    required this.onClearTap,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: results.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(results[index]),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () => onClearTap(index),
-                ),
-              );
-            },
-          ),
-        ),
-        if (results.isNotEmpty)
-          TextButton(
-            onPressed: () => onClearTap(null),
-            child: const Text('Clear All'),
-          ),
-      ],
-    );
-  }
-}
-
 void main() {
   runApp(const MyApp());
 }
@@ -80,9 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
   // Replace with your actual device name, service UUID, and characteristic UUID
   String searchDeviceName = 'Optical Pulse Oximeter'; 
   String searchServiceUUID = 'adf2a6e6-9b6d-4b5f-a487-77e21aafbc88';
-  String searchCharacteristicUUID = '2A37'; 
-
-  final List<String> _logs = [];
+  String searchCharacteristicUUID = '00002a37-0000-1000-8000-00805f9b34fb'; 
 
   @override
   void initState() {
@@ -95,29 +55,24 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _initializeBLE() async {
     AvailabilityState state = await UniversalBle.getBluetoothAvailabilityState();
     if (state != AvailabilityState.poweredOn) {
-      _addLog('Bluetooth is not enabled', '');
       return;
     }
 
     UniversalBle.onScanResult = (device) {
       print("Device found: ${device.name}"); // Log device name
       if (device.name == searchDeviceName) {
-        _addLog('Device found', device.name);
         _connectToDevice(device);
         UniversalBle.stopScan();
       }
     };
-    _addLog('Scanning for devices...', '');
     UniversalBle.startScan();
   }
 
   Future<void> _connectToDevice(BleDevice device) async {
     try {
-      _addLog('Connecting to device...', device.name);
       await UniversalBle.connect(device.deviceId); 
       // _discoverServices is now called in _onConnectionChange after successful connection
     } catch (e) {
-      _addLog('Error connecting to device: $e', '');
       setState(() {
         _connectionStatus = 'Error connecting'; 
       });
@@ -125,7 +80,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _onConnectionChange(String deviceId, bool isConnected, String? error) {
-    _addLog('Connection status changed', isConnected ? 'Connected' : 'Disconnected');
     setState(() {
       _connectedDeviceName = isConnected ? searchDeviceName : 'Unknown';
       _connectionStatus = isConnected ? 'Connected' : 'Disconnected';
@@ -140,17 +94,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _discoverServices(String deviceId) async {
     try {
-      _addLog('Discovering services...', '');
       List<BleService> services = await UniversalBle.discoverServices(deviceId);
-      _addLog('Services discovered:', services.length);
 
       for (var service in services) {
         if (service.uuid == searchServiceUUID) {
-          _addLog('Found service:', service.uuid);
           for (var characteristic in service.characteristics) {
             if (characteristic.uuid == searchCharacteristicUUID) {
-              _addLog('Found characteristic:', characteristic.uuid);
-
               // Enable notifications 
               await UniversalBle.setNotifiable(
                 deviceId,
@@ -158,27 +107,22 @@ class _MyHomePageState extends State<MyHomePage> {
                 searchCharacteristicUUID,
                 BleInputProperty.notification,
               );
-              _addLog('Notifications enabled for:', characteristic.uuid);
-              setState(() {
-                _receivedValue = characteristic.uuid; 
-    });
+              print('Notifications enabled for: ${characteristic.uuid}');
             }
           }
         }
       }
     } catch (e) {
-      _addLog('Error discovering services: $e', '');
+      print('Error discovering services: $e');
     }
   }
 
   void _onCharacteristicValueChange(
       String deviceId, String characteristicId, Uint8List value) {
     print('Raw data received: $value');
-    String receivedString = String.fromCharCodes(value);
-    _addLog('Received data:', receivedString);
 
     setState(() {
-      _receivedValue = receivedString; 
+      _receivedValue = value[1].toString(); 
     });
   }
 
@@ -187,17 +131,6 @@ class _MyHomePageState extends State<MyHomePage> {
     UniversalBle.disconnect(_connectedDeviceName);
     super.dispose();
   }
-
-  void _addLog(String type, dynamic data) {
-    if (mounted) {
-      setState(() {
-        _logs.add('$type: ${data.toString()}');
-      });
-    } else {
-      print('$type: ${data.toString()}'); // Tulostetaan loki konsoliin, jos setState ei ole mahdollinen
-    }
-  
-}
 
 
   @override
@@ -218,19 +151,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'Received Value: $_receivedValue',
               style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const Divider(),
-            ResultWidget(
-              results: _logs,
-              onClearTap: (int? index) {
-                setState(() {
-                  if (index != null) {
-                    _logs.removeAt(index);
-                  } else {
-                    _logs.clear();
-                  }
-                });
-              },
             ),
           ],
         ),
